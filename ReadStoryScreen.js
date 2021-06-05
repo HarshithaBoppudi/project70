183a124d7204045892a530135a8d91e5cab6aebe
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-06-05 11:05:04 +0530

    Add files via upload

--- ReadStoryScreen.js
+++ ReadStoryScreen.js
@@ -16,16 +16,14 @@ export default class ReadStoryScreen extends React.Component {
     const transactions=await db.collection('bedTimeStories').limit(10).get()
     transactions.docs.map((doc)=>{
         this.setState({
-          allStories:[],
+          allStories:[...this.state.allStories,doc.data()],
           
         })
     })
   }
   searchFilter=async(text)=>{
           var enteredText=text.split('')
-          
-
-
+        
   }
 render(){
   return(
@@ -40,8 +38,67 @@ render(){
     textAlign:"center",
     textDecorationLine:"underline",
     marginLeft:500,justifyContent:'center'}}>BED TIME STORIES</Text>
+
+<View style={styles.searchBar}>
+        <TextInput style={styles.bar}
+        placeholder='enter story name'
+        onChangeText={(text)=>{
+           this.setState({
+             search:text
+           })
+        }}
+        
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={()=>{
+          this.searchFilter(this.state.search)
+        }}>
+               <Text>Search</Text>
+        </TouchableOpacity>
+
+      </View>
+
+
+
+<FlatList
+       data={this.state.allStories}
+       renderItem={({item})=>(
+           <View style={{borderBottomWidth:2,marginTop:30,backgroundColor:'pink'}}>
+                 <Text>
+                   {'StoryName:  '+item.storyName}
+                 </Text>
+                 <Text>
+                   {'Author:  '+item.author}
+                 </Text>
+                 <Text>
+                   {'Story:  '+item.story}
+                 </Text>
+                
+           </View>
+       )}
+       keyExtractor={(item,index)=>index.toString()}
+      
+       />
     </View>
   )
 }
 
 }
+const styles=StyleSheet.create({
+  searchBar:{
+      flexDirection:'row',
+      height:40,
+      borderWidth:0.5,
+      alignItems:'center',
+      
+  },
+ 
+  searchButton:{
+    borderWidth:1,
+    height:30,
+    width:50,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'violet'
+
+  }
+})
